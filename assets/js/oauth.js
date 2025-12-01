afcc83a2ff6560e338e42ca63da3ccc163e75578
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-12-01 10:28:33 -0300

    refactor: Update OAuth button selectors and remove unused notification icons

--- assets/js/oauth.js
+++ assets/js/oauth.js
@@ -1,7 +1,7 @@
 const API_BASE_URL = window.API_BASE_URL || "http://localhost:3000/";
 
-const githubButton = document.querySelector(".github button");
-const googleButton = document.querySelector(".google button");
+const githubButton = document.querySelector(".oauth-button github-button");
+const googleButton = document.querySelector(".oauth-button google-button");
 const GITHUB_CLIENT_ID = "Ov23li2t6eaF0ZJAgV7L";
 const GOOGLE_CLIENT_ID =
   "133503123644-8to0kqg77hc2rpc1ga0l5f4am3lt8rlj.apps.googleusercontent.com ";
